d30ca9808615cc77bc5aaf31aa453aa66853a9ce
DavinTandy <dav.tandy@gmail.com>
2024-05-25 11:32:14 +0700

    Add scoring logic

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 const choices = ["Rock", "Paper", "Scissors"];
+let playerScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
     const computerChoice = choices[Math.floor(Math.random() * 3)];
@@ -19,8 +21,12 @@ function getPlayerChoice() {
 function playRound(computerChoice, playerChoice) {
     if (computerChoice === "Rock" && playerChoice === "Paper" || computerChoice === "Paper" && playerChoice === "Scissors" || computerChoice === "Scissors" && playerChoice === "Rock") {
         document.getElementById("result").innerText = "You win";
+        playerScore += 1;
+        document.getElementById("player-score").innerText = "Player Score: " + playerScore;
     } else if (computerChoice === "Rock" && playerChoice === "Scissors" || computerChoice === "Paper" && playerChoice === "Rock" || computerChoice === "Scissors" && playerChoice === "Paper") {
         document.getElementById("result").innerText = "Computer win";
+        computerScore += 1;
+        document.getElementById("computer-score").innerText = "Computer Score: " + computerScore;
     } else {
         document.getElementById("result").innerText = "Draw";
     }
