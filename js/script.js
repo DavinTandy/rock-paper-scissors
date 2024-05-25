1bd64266ef2aeb178b7ee4560a8ad998a8395020
DavinTandy <dav.tandy@gmail.com>
2024-05-25 11:16:51 +0700

    Add player choice logic

--- js/script.js
+++ js/script.js
@@ -4,3 +4,14 @@ function getComputerChoice() {
     const computerChoice = choices[Math.floor(Math.random() * 3)];
     return computerChoice;
 }
+
+function getPlayerChoice() {
+    const playerChoices = document.getElementsByName('choice');
+
+    for (i = 0; i < playerChoices.length; i++) {
+        if (playerChoices[i].checked) {
+            const playerChoice = playerChoices[i].value;
+            return playerChoice;
+        }
+    }
+}
